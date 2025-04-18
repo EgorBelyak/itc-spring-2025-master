e4c02b6713d792e8c638cdea37d6e74ac4becbf1
ESBelyak <ESBelyak@yandex.ru>
2025-04-18 18:31:33 +0500

    04

---
+++ 04/src/store/reducers/KnightSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  row: 0,
+  column: 0,
+};
+
+export const knightSlice = createSlice({
+  name: 'knight',
+  initialState,
+
+  reducers: {
+    setPosition: (state, action) => {
+      state.row = action.payload.row;
+      state.column = action.payload.column;
+    },
+  },
+  selectors: {
+    getRow: (state) => state.row,
+    getColumn: (state) => state.column,
+  },
+});
+
+export const { setPosition } = knightSlice.actions;
+export const { getRow, getColumn } = knightSlice.selectors;
+
+export default knightSlice.reducer;
